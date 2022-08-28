6e7dc618b0bc13b22ddc71c3df3363dc1479988f
Jiorm <hiehiohi@gmail.com>
2022-08-28 21:35:43 +0500

    cleaning project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {AuthContext} from "./context";
+import axios from "axios";
 
-function App() {
+const App = () => {
+    useEffect(() => {
+        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Ekaterinburg&appid=d0a89c0555a1454404f6c8d8a4f4f90f&units=metric')
+            .then(res => {
+                console.log(res.data);
+            })
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div>
+          </div>
   );
-}
+};
 
 export default App;
