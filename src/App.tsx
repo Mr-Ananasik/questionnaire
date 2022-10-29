545d9a2b7de02f7fd07c4d7ba518a1f4115eb557
Jiorm <hiehiohi@gmail.com>
2022-10-29 13:36:47 +0500

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from 'react'
 import MyButton from './components/Button'
 
 const App = () => {
-  console.log('123');
-  return (
-          <div>
-              <MyButton width={"150px"} textColor={'white'} bgColor={'green'}>Click!</MyButton>
-          </div>
-  );
-};
+	return (
+		<div>
+			<MyButton width={'150px'} textColor={'white'} bgColor={'green'}>
+				Click!
+			</MyButton>
+		</div>
+	)
+}
 
-export default App;
+export default App
